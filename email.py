87542f45092ad09623291734c8c90d0bbdfaccf0
dheerajboddu <dheeraj7121997@gmail.com>
2023-02-09 17:03:40 -0500

    Add files via upload

---
+++ email.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  9 16:40:13 2023
+
+@author: Dheeraj Boddu
+"""
+
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+e_s = 'dheeraj7121997@gmail.com'
+e_p = "zrsgymshilruheic"
+e_r = "boddudheerajkumar@gmail.com"
+
+sub = "hello Docker"
+bod = "Hi this message is automated from docker"
+
+em = EmailMessage()
+
+em['From'] = e_s
+em['To'] = e_r
+em['Subject'] = sub
+em.set_content(bod)
+
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context= context) as smpt:
+    smpt.login(e_s,e_p)
+    smpt.sendmail(e_s,e_r,em.as_string())
